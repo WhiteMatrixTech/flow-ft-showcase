dde50a390bd99e3b61d5f57f80786cd0af0df53b
Hexi1997 <18715156450@163.com>
2024-08-27 15:34:50 +0800

    feat: update script

--- frontend/wallet/cadence/get_fusd_balance.ts
+++ frontend/wallet/cadence/get_fusd_balance.ts
@@ -1,7 +1,7 @@
 export const getFusdBalanceScript = `
 import FUSD from 0xFUSD_ADDRESS
-import FungibleToken from 0x9a0766d93b6608b7 //Mainnet Address: 0xf233dcee88fe0abe
-import FungibleTokenMetadataViews from 0x9a0766d93b6608b7 //Mainnet address: 0xf233dcee88fe0abe
+import FungibleToken from 0xFUNGIBLE_TOKEN_ADDRESS
+import FungibleTokenMetadataViews from 0xFUNGIBLE_TOKEN_ADDRESS
 
 access(all) fun main(address: Address): UFix64 {
     let vaultData = FUSD.resolveContractView(resourceType: nil, viewType: Type<FungibleTokenMetadataViews.FTVaultData>()) as! FungibleTokenMetadataViews.FTVaultData?
